6f640d6b8a4291b71d7f81b4048aee9047532da3
21011996 <21011996@mail.ru>
2016-09-17 14:27:06 +0300

    init

---
+++ src/Dot.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Ilya239 on 17.09.2016.
+ */
+public class Dot {
+    double x;
+    double y;
+    int lul;
+
+    Dot(double x, double y, int lul) {
+        this.x = x;
+        this.y = y;
+        this.lul = lul;
+    }
+}

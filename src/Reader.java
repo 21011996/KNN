2e096a1cf5d7b5522e0d13d750d580e14f009ed9
shambala22 <asmavrin@rain.ifmo.ru>
2016-09-17 14:46:43 +0300

    reader add

---
+++ src/Reader.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.ArrayList;
+
+/**
+ * Created by shambala on 17/09/16.
+ */
+
+public class Reader {
+    public ArrayList<Dot> read(String file) {
+        ArrayList<Dot> dots = new ArrayList<>();
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] spl = line.split(",");
+                dots.add(new Dot(Double.parseDouble(spl[0]), Double.parseDouble(spl[1]), Integer.parseInt(spl[2])));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return dots;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Dot> dots = new Reader().read("chips.txt");
+    }
+}
+
